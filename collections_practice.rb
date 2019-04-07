8d7e097d9fe77641ba5505f2bfbc7d0dbacbacf9
Codey Pinero <codey.pinero@gmail.com>
2019-04-07 17:09:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+require 'pry'
 
+def sort_array_asc(num_array)
+  num_array.sort
+end
+
+def sort_array_desc(num_array)
+  num_array.sort.reverse
+end
+
+def sort_array_char_count(string_array)
+  string_array.sort_by {|string| string.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+  #binding.pry
+end
+
+def reverse_array(int_array)
+  int_array.reverse
+end
+
+def kesha_maker(array)
+  kesh_array = array.each do |word|
+    word[2] = "$"
+  end
+  kesh_array
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(word_array)
+  words_array = word_array.each do |word|
+    next if word == word_array[1]
+    p word + "s"
+  end
+  binding.pry
+end
+
+binding.pry
